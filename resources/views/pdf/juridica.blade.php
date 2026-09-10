680896659a4937c9df2f512b32903a4aeb91f584
diegomrWork <diego.miranda@autonort.com.pe>
2026-09-10 12:55:44 -0500

    Update juridica.blade.php
    Espacios

--- resources/views/pdf/juridica.blade.php
+++ resources/views/pdf/juridica.blade.php
@@ -250,13 +250,12 @@
     <p>Atentamente,</p>
 
     <br><br>
-    <br><br>
-    <br><br>
+    <br>
     <div class="caja-firma">
         <p class="uppercase" style="margin: 0;">{{ $datos['juridica']['nombre_representante'] }}</p>
         <p style="margin: 0;">{{ $datos['juridica']['tipo_doc_representante'] }} N° {{ $datos['juridica']['dni_representante'] }}</p>
     </div>
-    <br><br><br><br><br><br><br><br>
+    <br><br><br><br><br>
 
     <div style="margin-top: 40px; text-align: justify; font-size: 10px;">
         <p class="uppercase">CERTIFICO:<br>
