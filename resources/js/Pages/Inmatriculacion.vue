d5ffa0ec177cf58796a1922dfcb1e407a5f95d29
diegomrWork <diego.miranda@autonort.com.pe>
2026-09-10 13:03:28 -0500

    Update Inmatriculacion.vue
    Condicional de Juridica
    Partida Registra y lugar no obligatorio

--- resources/js/Pages/Inmatriculacion.vue
+++ resources/js/Pages/Inmatriculacion.vue
@@ -108,7 +108,7 @@
                         <div class="grid grid-cols-2 gap-4 mb-4">
                             <div>
                                 <label class="block text-gray-700">N° de Partida Registral:</label>
-                                <input type="text" v-model="form.juridica.partida" class="w-full border-gray-300 rounded-md shadow-sm" required />
+                                <input type="text" v-model="form.juridica.partida" class="w-full border-gray-300 rounded-md shadow-sm" />
                             </div>
                             
                             <!-- NUEVO BUSCADOR DE PROVINCIA REGISTRAL -->
@@ -120,7 +120,7 @@
                                         @focus="mostrarDropdownProvincia = true"
                                         @blur="validarProvincia"
                                         class="w-full border-gray-300 rounded-md shadow-sm pr-10 uppercase" 
-                                        placeholder="Seleccione..." required autocomplete="off" />
+                                        placeholder="Seleccione..." autocomplete="off" />
                                     
                                     <div class="absolute right-3 top-3 pointer-events-none text-gray-500">
                                         <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
